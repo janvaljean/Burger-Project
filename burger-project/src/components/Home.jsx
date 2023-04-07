da1643953787472228eaabc6e5f2e7d8f4d70bee
janvaljean <kurtuldu@gmx.de>
2023-04-07 04:30:06 +0200

    V5 Home created and css added

--- burger-project/src/components/Home.jsx
+++ burger-project/src/components/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import "../styles/Home.css";
+import BannerImage from "../assets/banneryeni.jpg"
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      home
+    <div
+      style={{ backgroundImage: `url(${BannerImage})` }}
+      className="mainPage"
+    >
+      <div className="order">
+        <Link to="/menu">
+          <button>Make an Order</button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
