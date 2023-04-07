6c945dbd7d8136d9c1be63982cf54c442508f61b
janvaljean <kurtuldu@gmx.de>
2023-04-07 03:56:42 +0200

    V4 footer done

--- burger-project/src/components/Footer.jsx
+++ burger-project/src/components/Footer.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+import "../styles/Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      footer
+    <div className="footer">
+      <div className="socialMedia">
+        <FacebookIcon />
+        <InstagramIcon />
+        <TwitterIcon />
+      </div>
+      <p className="footerText">
+        <CopyrightIcon />
+        All Rights Reserved | Sefa
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
