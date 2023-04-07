004e08e89e2fca4bee89866370f31771aa82e7dd
janvaljean <kurtuldu@gmx.de>
2023-04-07 03:40:49 +0200

    V2  navbar css added and links created

--- burger-project/src/App.jsx
+++ burger-project/src/App.jsx
@@ -4,16 +4,20 @@ import About from './components/About';
 import Home from "./components/Home";
 import Menu from "./components/Menu";
 import Contact from "./components/Contact";
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/menu" exact element={<Menu />} />
         <Route path="/about" exact element={<About />} />
         <Route path="/contact" exact element={<Contact />} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
--- burger-project/src/components/Navbar.jsx
+++ burger-project/src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import BurgerLogo from "../assets/burgerlogo.png"
+import BurgerLogo from "../assets/burgerlogo.png";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="main">
         <img src={BurgerLogo} alt="" />
+      </div>
+      <div className="mainLink">
+        <Link to="/">Homepage</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
       </div>
     </div>
   );
